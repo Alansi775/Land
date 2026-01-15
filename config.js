7f6505d74c1d7094c37e22d82e7ef2a6ae9783a0
Mohammed <mackbook@Mohammeds-Mackbook-MacBook-Air.local>
2026-01-15 18:04:25 +0300

    images test

--- config.js
+++ config.js
@@ -13,6 +13,6 @@ const API_CONFIG = (() => {
     
     // إذا كان GitHub Pages أو أي domain آخر → استخدم ngrok
     return {
-        apiUrl: 'https://02131f70f302.ngrok-free.app/api'
+        apiUrl: 'https://74d2d673d808.ngrok-free.app/api'
     };
 })();
